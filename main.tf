59a4e593d7a05ace5e06e6b46a543616e72a8b2a
learnit06 <vareshrajvallala@gmail.con>
2025-02-06 15:21:39 +0000

    changed public ip method from 'Dynamic to 'Static'

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_public_ip" "gw_pip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_virtual_network_gateway" "vnet_gateway" {
